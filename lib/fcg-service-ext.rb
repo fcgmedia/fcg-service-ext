fca30fd36ea81b4f0409329f5a6d0a15481890e9
Samuel O. Obukwelu <sam@fcgmedia.com>
2010-09-03 04:07:47 -0400

    fixed minor bug

--- lib/fcg-service-ext.rb
+++ lib/fcg-service-ext.rb
@@ -1,4 +1,4 @@
 require 'fcg-service-ext/version'
 require 'fcg-service-ext/validation_constants'
-require 'fcg-service-ext/service'
+require 'fcg-service-ext/fcg-service'
 require 'fcg-service-ext/mongo_mapper/plugins'
