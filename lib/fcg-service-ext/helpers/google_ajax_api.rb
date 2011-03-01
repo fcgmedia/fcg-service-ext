6bce86d1ecb880a5719b80c3946350d7f77c2f85
Samuel O. Obukwelu <sam@fcgmedia.com>
2011-03-01 14:26:08 -0500

    updated to 0.1.2

--- lib/fcg-service-ext/helpers/google_ajax_api.rb
+++ lib/fcg-service-ext/helpers/google_ajax_api.rb
@@ -1,24 +1,40 @@
 module FCG
   module GoogleAjaxLibApi
-    def load_jquery(debug, jquery_version="1.4.2")
-      if debug
-        javascript_include_tag "jquery/jquery-#{jquery_version}", "jquery/jquery-ui-1.8.2.custom", "jquery/jquery.getJs"
+    def load_jquery(*args)
+      opts = args.extract_options!
+      opts = { 
+        :debug => false,
+        :jquery_version => "1.5",
+        :jquery_ui_version => "1.8.9"
+      }.merge(opts)
+      
+      if opts[:debug]
+        javascript_include_tag "jquery/jquery-#{opts[:jquery_version]}", "jquery/jquery-ui-#{opts[:jquery_ui_version]}.custom", "jquery/jquery.getJs"
       else
       <<-HTML
       <script src="#{request.protocol}www.google.com/jsapi"></script>
       <script>
         // <![CDATA[
-        google.load("jquery", "#{jquery_version}");
+        google.load("jquery", "#{opts[:jquery_version]}");
+        google.load("jqueryui", "#{opts[:jquery_ui_version]}");
         // ]]>
       </script>
-      #{javascript_include_tag "jquery/jquery-ui-1.8.2.custom", "jquery/jquery.getJs", :cache => false}
+      #{javascript_include_tag "jquery/jquery.getJs", :cache => false}
       HTML
       end
     end
 
-    def load_google_map(key)
+    def load_google_map(key, *args)
+      opts = args.extract_options!
+      opts = { 
+        :url => "maps.google.com/maps",
+        :file => "api",
+        :v => "2",
+        :sensor => "false"
+      }.merge(opts)
+      
       <<-HTML
-      <script src="#{request.protocol}maps.google.com/maps?file=api&amp;v=2&amp;sensor=false&amp;key=#{key}"></script>
+      <script src="#{request.protocol}#{opts[:url]}?file=#{opts[:file]}&amp;v=#{opts[:v]}&amp;sensor=#{opts[:sensor]}&amp;key=#{key}"></script>
       HTML
     end
   end
