5bc4c8bc64039576c120938dca7172558c8a59ce
Samuel O. Obukwelu <sam@fcgmedia.com>
2011-03-01 16:16:38 -0500

    updated session model and bumped version up to 0.1.5

--- lib/fcg-service-ext/models/session.rb
+++ lib/fcg-service-ext/models/session.rb
@@ -4,7 +4,6 @@ module FCG
   module Models
     class Session
       include FCG::Client::Session
-      setup_service :hydra => FCG::Client::Base::HYDRA, :host => FCG_CONFIG.app.service_url, :version => FCG_CONFIG.app.service_version
     end
   end
 end
